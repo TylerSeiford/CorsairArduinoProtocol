02571fb89e753e12aaf91c1df22e45e30ac819f9
TylerSeiford <TylerSeiford@users.noreply.github.com>
2019-06-20 16:59:49 -0500

    Adding fan type to fan interface

--- src/IFan.h
+++ src/IFan.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "Arduino.h"
+#include "FanController.h"
 
 class IFan {
 public:
-	IFan(uint16_t minRPM, uint16_t maxRPM) : minRPM(minRPM), maxRPM(maxRPM) {}
+	IFan(uint16_t minRPM, uint16_t maxRPM, uint8_t type) : minRPM(minRPM), maxRPM(maxRPM), type(type) {}
+	IFan(uint16_t minRPM, uint16_t maxRPM) : minRPM(minRPM), maxRPM(maxRPM), type(FAN_MASK_DISCONNECTED) {}
 	virtual void setPower(uint8_t percentage);
 	uint8_t calculatePowerFromSpeed(uint16_t rpm);
 	uint16_t calculateSpeedFromPower(uint8_t power);
+
+	const uint8_t type;
 protected:
 	const uint16_t minRPM;
 	const uint16_t maxRPM;
